e69d799491346a454d0d4e8191f63c920c8bb708
jdev <20902932+osekojoe@users.noreply.github.com>
2025-01-24 08:30:02 +0100

    tweaks

--- R1.R
+++ R1.R
@@ -39,7 +39,7 @@ index <- c(1:n)
 
 set.seed(2025)
 
-B <- 1000
+B <- 10000
 LRT.nb <- c(1:B)
 
 for (i in 1:B) {
@@ -57,12 +57,12 @@ for (i in 1:B) {
 
 png("pictures/histnonparam1.2.png", width = 18, 
     height = 10, units = "cm", res = 300)
-hist(LRT.nb, nclass=50, xlim=c(0,30), xlab="LRT.b", main = "Histogram of LRT.b")
-lines(c(LRT.obs,LRT.obs) ,c(0,500),col=2)
+hist(LRT.nb, nclass=50, xlim=c(0,40), xlab="LRT.b", main = "Histogram of LRT.b")
+lines(c(LRT.obs,LRT.obs) ,c(0,5300),col=2)
 dev.off()
 
 #(pval.nonp <- mean(LRT.nb >= LRT.obs)) # 0.759
-pval.nonp <- (1 + sum(LRT.nb > LRT.obs)) / (B+1) # 0.759
+pval.nonp <- (1 + sum(LRT.nb > LRT.obs)) / (B+1) # 0.7442256
 pval.nonp
 ## -------------------------------------------------------------------
 # parametric
@@ -71,10 +71,10 @@ Satellites <- crabs$Satellites
 Width <- crabs$Width
 GoodSpine <- crabs$GoodSpine
 
-hat.mu<-mean(Satellites)
+(hat.mu<-mean(Satellites))
 
 set.seed(2025)
-B <- 1000  # Number of bootstrap samples
+B <- 10000  # Number of bootstrap samples
 LRT.pb <- c(1:B)
 
 for (i in 1:B) {
@@ -88,16 +88,16 @@ for (i in 1:B) {
 
 png("pictures/histparam1.2.png", width = 18, 
     height = 10, units = "cm", res = 300)
-hist(LRT.pb, nclass=50, xlim=c(-1,13), xlab="LRT.b", main = "Histogram of LRT.b")
-lines(c(LRT.obs, LRT.obs) ,c(0,500),col=2)
+hist(LRT.pb, nclass=50, xlim=c(-1,20), xlab="LRT.b", main = "Histogram of LRT.b")
+lines(c(LRT.obs, LRT.obs) ,c(0,5300),col=2)
 dev.off()
 
-(pval.p <- (1 + sum(LRT.pb > LRT.obs)) / (B+1)) # 0.556
+(pval.p <- (1 + sum(LRT.pb > LRT.obs)) / (B+1)) # 0.5624376
 
 ## -------------------------------------------------------------------
 # permutation
 set.seed(2025)
-B <- 1000  # Number of bootstrap samples
+B <- 10000  # Number of bootstrap samples
 LRT.permb <- c(1:B)
 
 for (i in 1:B) {
@@ -113,8 +113,8 @@ for (i in 1:B) {
 png("pictures/histperm1.2.png", width = 18, 
     height = 10, units = "cm", res = 300)
 hist(LRT.permb, nclass=50, xlim=c(0,40), xlab="LRT.b", main = "Histogram of LRT.b")
-lines(c(LRT.obs,LRT.obs) ,c(0,500),col=2)
+lines(c(LRT.obs,LRT.obs) ,c(0,5300),col=2)
 dev.off()
 
-(pval.p <- (1 + sum(LRT.permb > LRT.obs)) / (B+1))  # 0.747
+(pval.p <- (1 + sum(LRT.permb > LRT.obs)) / (B+1))  # 0.7472527
 
